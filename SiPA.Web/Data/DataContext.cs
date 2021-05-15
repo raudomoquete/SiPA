44f627a4584c1ed9b32d225fec04e2feda55efe3
Raudo Moquete <raudo.moquete@rhodium.com.do>
2021-05-15 11:50:21 -0400

    database created with user as first entitie. also migration as initialDB where created

---
+++ SiPA.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SiPA.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiPA.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ SiPA.Web/Data/Entities/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiPA.Web.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        [Display(Name = "Cell Phone")]
+        public string PhoneNo { get; set; }
+
+        [Display(Name = "User")]
+        public string FullName => $"{FirstName} {LastName}";
+
+    }
+}
