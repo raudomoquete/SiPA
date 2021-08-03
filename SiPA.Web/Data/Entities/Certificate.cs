d47ca67d5b93f65e34aa8f4a609369de2f3b6b6f
Raudo Moquete <raudo.moquete@rhodium.com.do>
2021-08-03 17:45:49 -0400

    changes on certificate entitie

--- SiPA.Web/Data/Entities/Certificate.cs
+++ SiPA.Web/Data/Entities/Certificate.cs
@@ -9,10 +9,12 @@ namespace SiPA.Web.Data.Entities
     public class Certificate
     {
         public int Id { get; set; }
-        public string Document { get; set; }
 
         [Display(Name = "Nombre")]
-        public string Name { get; set; }
+        public string Tipo { get; set; }
+
+        [Display(Name = "Feligrés")]
+        public Parishioner Parishioner { get; set; }
 
         [Display(Name = "Descripción")]
         public string Description { get; set; }
@@ -21,9 +23,8 @@ namespace SiPA.Web.Data.Entities
         public string Comments { get; set; }
 
         [Display(Name = "Fecha en que se expide")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? issuedDate { get; set; }
-        public Parishioner Parishioner { get; set; }
-        public DateTime? UpdatedAt { get; set; }
-        public DateTime? CreatedAt { get; set; }
     }
 }
